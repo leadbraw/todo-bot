065bf9b73e56bdea728e360d3dfdf64bc89d0a06
Noah <noah.dangerfield@gmail.com>
2025-03-10 14:06:23 -0700

    Command handling

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 
 import fs from 'node:fs';
 import path from 'node:path';
-import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Collection, Events, GatewayIntentBits, MessageFlags } from 'discord.js';
 import { token } from './config.json';
 
 // Create a new client instance
@@ -29,9 +29,32 @@ for (const folder of commandFolders) {
 }
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
+// It makes some properties non-nullable. 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) {
+        return;
+    }
+	const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: "There was an error while executing the command!", flags: MessageFlags.Ephemeral });
+        } else {
+            await interaction.reply({ content: "There was an error while executing the command!", flags: MessageFlags.Ephemeral });
+        }
+    }
 });
 
 // Log in to Discord with your client's token
