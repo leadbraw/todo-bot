0e831159faf1597d8899ce947102dd6d44efd4c6
Noah <noah.dangerfield@gmail.com>
2025-03-10 14:35:44 -0700

    Moved event handlers out, added code to fetch them in index.ts

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ client.commands= new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
+// Load all commands
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
@@ -27,35 +28,20 @@ for (const folder of commandFolders) {
 		}
 	}
 }
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable. 
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
 
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) {
-        return;
-    }
-	const command = interaction.client.commands.get(interaction.commandName);
+//  Load all event handlers
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: "There was an error while executing the command!", flags: MessageFlags.Ephemeral });
-        } else {
-            await interaction.reply({ content: "There was an error while executing the command!", flags: MessageFlags.Ephemeral });
-        }
-    }
-});
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 // Log in to Discord with your client's token
 client.login(token);
