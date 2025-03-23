5a208859458025a7f3392e5d29dfeaae7392fd0d
Noah <124291209+leadbraw@users.noreply.github.com>
2025-03-23 14:38:54 -0700

    Added echo command.

---
+++ commands/util/echo.ts
@@ -0,0 +1,20 @@
+import { MessageFlags, SlashCommandBuilder } from 'discord.js';
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('echo')
+		.setDescription('Repeats a message back to you!')
+        .addStringOption(option =>
+            option.setName('input')
+                .setDescription('The input to echo back.')
+                .setRequired(true))
+        .addBooleanOption(option =>
+            option.setName('ephemeral')
+                .setDescription('Whether the output should be ephemeral. True by default.')
+                .setRequired(false)),
+	async execute(interaction) {
+        const ephemeralValue: boolean = interaction.options.getBoolean('ephemeral') ?? true;
+        const inputMessage: String = interaction.options.getString('input');
+		await interaction.reply({content: inputMessage, flags: ephemeralValue ? MessageFlags.Ephemeral : undefined});
+	},
+};
--- commands/util/ping.ts
+++ commands/util/ping.ts
@@ -3,7 +3,7 @@ import { SlashCommandBuilder } from 'discord.js';
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Gives the bot\'s ping'),
+		.setDescription('Gives the bot\'s ping.'),
 	async execute(interaction) {
 		const sent = await interaction.reply('Pong!');
         interaction.editReply(`Pong!\nWebsocket heartbeat: ${interaction.client.ws.ping} ms\nRoundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp} ms`)
