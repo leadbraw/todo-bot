b53da042ab45a67569bf56901822cdea57b8dc7d
Noah <124291209+leadbraw@users.noreply.github.com>
2025-03-23 17:37:29 -0700

    Began work on converting /server response to embed.

--- commands/util/server.ts
+++ commands/util/server.ts
@@ -1,10 +1,29 @@
-import {SlashCommandBuilder} from 'discord.js';
+import { EmbedBuilder, GuildTextBasedChannel, SlashCommandBuilder, TextBasedChannel } from 'discord.js';
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
 		.setDescription('Provides information about the server.'),
 	async execute(interaction) {
+        const channel: GuildTextBasedChannel = interaction.channel;
+        const serverEmbed = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle(interaction.guild.name)
+            .setURL('https://github.com/leadbraw')
+            .setAuthor({ name: interaction.user.username, iconURL: interaction.user.defaultAvatarURL })
+            .setDescription('Some description here')
+            .setThumbnail('https://i.imgur.com/OrNGRgy.png')
+            .addFields(
+                { name: 'Regular field title', value: 'Some value here' },
+                { name: '\u200B', value: '\u200B' },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+            )
+            .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+            .setImage('https://i.imgur.com/OrNGRgy.png')
+            .setTimestamp()
+            .setFooter({ text: interaction.guild.name, iconURL: 'https://i.imgur.com/OrNGRgy.png' });
+        channel.send({ embeds: [serverEmbed] });
 		// interaction.guild is the object representing the Guild in which the command was run
 		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
 	},
