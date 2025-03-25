760d3f1596044e2be79665922baf63208d2583cf
Noah <124291209+leadbraw@users.noreply.github.com>
2025-03-24 21:49:10 -0700

    Probably done touching /server for now

--- commands/util/server.ts
+++ commands/util/server.ts
@@ -1,4 +1,4 @@
-import { EmbedBuilder, GuildTextBasedChannel, SlashCommandBuilder, TextBasedChannel } from 'discord.js';
+import { EmbedBuilder, Guild, GuildTextBasedChannel, SlashCommandBuilder, TextBasedChannel } from 'discord.js';
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -6,28 +6,29 @@ module.exports = {
 		.setDescription('Provides information about the server.'),
 	async execute(interaction) {
         const channel: GuildTextBasedChannel = interaction.channel;
+        const guild: Guild = interaction.guild;
         const serverEmbed = new EmbedBuilder()
             .setColor(0x0099FF)
-            .setTitle(interaction.guild.name)
+            .setTitle(guild.name)
             .setURL('https://github.com/leadbraw')
             .setAuthor({ name: interaction.user.username, iconURL: interaction.user.avatarURL() })
-            // TODO: make this line shorter :v
-            .setDescription(`Creation date: <t:${Math.floor(interaction.guild.createdAt.getTime() / 1000)}:D> (<t:${Math.floor(interaction.guild.createdAt.getTime() / 1000)}:R>)`)
-            .setThumbnail(interaction.guild.iconURL())
+            // TODO: Make this line shorter. Displays creation date via discord timestamps.
+            .setDescription(`Creation date: <t:${Math.floor(guild.createdAt.getTime() / 1000)}:D> (<t:${Math.floor(guild.createdAt.getTime() / 1000)}:R>)`)
+            .setThumbnail(guild.iconURL())
             .addFields(
                 // first row
-                { name: 'Owner', value: `<@${interaction.guild.ownerId}>`, inline: true },
-                { name: 'Members', value: `${interaction.guild.memberCount}`, inline: true },
-                { name: 'Channels', value: `${interaction.guild.channels.channelCountWithoutThreads}`, inline: true},
+                { name: 'Owner', value: `<@${guild.ownerId}>`, inline: true },
+                { name: 'Members', value: `${guild.memberCount}`, inline: true },
+                { name: 'Channels', value: `${guild.channels.channelCountWithoutThreads}`, inline: true},
                 // second row
-                { name: 'Design', value: `Icon: [Click here](${interaction.guild.iconURL()})`, inline: true},
-                { name: 'Info', value: `Verification: ${interaction.guild.verificationLevel}
-                                        Boosts: ${interaction.guild.premiumSubscriptionCount}`, inline: true},
-                { name: 'Counts', value: `Roles: ${interaction.guild.roles.cache.size}
-                                          Emojis: ${interaction.guild.emojis.cache.size}`, inline: true}
+                { name: 'Design', value: `Icon: [Click here](${guild.iconURL()})`, inline: true},
+                { name: 'Info', value: `Verification: ${guild.verificationLevel}
+                                        Boosts: ${guild.premiumSubscriptionCount}`, inline: true},
+                { name: 'Counts', value: `Roles: ${guild.roles.cache.size}
+                                          Emojis: ${guild.emojis.cache.size}`, inline: true}
             )
             .setTimestamp()
-            .setFooter({ text: `Guild ID: ${interaction.guild.id}` });
+            .setFooter({ text: `Guild ID: ${guild.id}` });
         interaction.reply({ embeds: [serverEmbed] });
 	}
 };
