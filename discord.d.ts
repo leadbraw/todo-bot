6a89620047d2ff3ee7a1cecd03092ecad3cd64fc
Noah <124291209+leadbraw@users.noreply.github.com>
2025-04-17 20:13:36 -0700

    Update discord.d.ts

--- discord.d.ts
+++ discord.d.ts
@@ -1,6 +1,5 @@
 import { Collection } from "discord.js";
 
-// Made to satisfy my IDE. Now client has a commands property.
 declare module "discord.js" {
     export interface Client {
         commands: Collection<unknown, any>
